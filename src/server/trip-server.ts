7a450c4b909aead106e5a27fcf2d58e093bde096
matheusdonizetiferreira <mathferreira726@gmail.com>
2024-07-11 14:43:16 -0300

    atualização de algumas pastas

--- src/server/trip-server.ts
+++ src/server/trip-server.ts
@@ -1,23 +1,23 @@
-import { api } from "./api";
+import { api } from "./api"
 
 export type TripDetails = {
-  id: string;
-  destination: string;
-  starts_at: string;
-  ends_at: string;
-  is_confirmed: boolean;
-};
+  id: string
+  destination: string
+  starts_at: string
+  ends_at: string
+  is_confirmed: boolean
+}
 
 type TripCreate = Omit<TripDetails, "id" | "is_confirmed"> & {
-  emails_to_invite: string[];
-};
+  emails_to_invite: string[]
+}
 
 async function getById(id: string) {
   try {
-    const { data } = await api.get<{ trip: TripDetails }>(`/trips/${id}`);
-    return data.trip;
+    const { data } = await api.get<{ trip: TripDetails }>(`/trips/${id}`)
+    return data.trip
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
@@ -29,35 +29,35 @@ async function create({
 }: TripCreate) {
   try {
     const { data } = await api.post<{ tripId: string }>("/trips", {
-        destination,
-        starts_at,
-        ends_at,
-        emails_to_invite,
-        owner_name: "Matheus Ferreira",
-        owner_email: "mathferreira726@gmail.com",
+      destination,
+      starts_at,
+      ends_at,
+      emails_to_invite,
+      owner_name: "Rodrigo Gonçalves",
+      owner_email: "rodrigo.rgtic@gmail.com",
     })
 
     return data
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
 async function update({
-   id,
-   destination,
-    starts_at,
-     ends_at,
-     }: Omit<TripDetails, "is_confirmed">){
-      try {
-        await api.put(`/trips/${id}`,{
-          destination,
-          starts_at,
-           ends_at
-        })
-      } catch (error) {
-        throw error
-      }
+  id,
+  destination,
+  starts_at,
+  ends_at,
+}: Omit<TripDetails, "is_confirmed">) {
+  try {
+    await api.put(`/trips/${id}`, {
+      destination,
+      starts_at,
+      ends_at,
+    })
+  } catch (error) {
+    throw error
+  }
 }
 
-export const tripServer = { getById, create, update };
+export const tripServer = { getById, create, update }
